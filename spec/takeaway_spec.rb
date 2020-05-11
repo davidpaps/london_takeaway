3b94ae5fed93394777f940ab924ab2035adcbb48
David Papamichael <davidpapamichael@gmail.com>
2020-05-11 16:33:59 +0100

    finalise order method feature tested in Takeaway

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,11 +2,12 @@ require 'takeaway'
 
 describe Takeaway do
   
- 
   menu = Menu.new
-  order = Order.new
   message = Message.new
+  order = Order.new(menu, message)
   subject = described_class.new(order)
+ 
+  
 
   describe "#view_menu" do
     it 'allows a user to view the menu, default if one is not provided' do
@@ -15,15 +16,24 @@ describe Takeaway do
   end
 
   describe '#order_item' do
-
     it "allows a user to order items on the menu" do
       subject.order_item("curry", 2)
       subject.order_item("kebab", 1)
       expect(subject.order.basket.length).to eq(2)
       expect(subject.order_item("pizza")).to eq("1 x Pizza Added, Total = £41")
     end
+  end
 
-    
+  describe '#finalise_order' do
+      it "sends text with order details" do
+        expect(message).to receive(:send)
+        subject.finalise_order
+      end
+
+      it 'see a final confirmation of order when actioned' do
+        expect(message).to receive(:send)
+        expect(subject.finalise_order).to eq('Order = 2 x Curry, 1 x Kebab, 1 x Pizza, Total = £41')
+      end
   end
 
 end
