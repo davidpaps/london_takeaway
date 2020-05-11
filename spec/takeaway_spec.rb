33939eae176d0ad30b316fdc3189c85dbff5f0f8
David Papamichael <davidpapamichael@gmail.com>
2020-05-11 16:12:48 +0100

    order_item feature tested in takeaway class"

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -12,7 +12,17 @@ describe Takeaway do
     it 'allows a user to view the menu, default if one is not provided' do
       expect(subject.view_menu).to eq("pizza = £10, kebab = £9, curry = £11")
     end
-   
+  end
+
+  describe '#order_item' do
+
+    it "allows a user to order items on the menu" do
+      subject.order_item("curry", 2)
+      subject.order_item("kebab", 1)
+      expect(subject.order.basket.length).to eq(2)
+      expect(subject.order_item("pizza")).to eq("1 x Pizza Added, Total = £41")
+    end
+
     
   end
 
