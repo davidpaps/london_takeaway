600dcff02658cd5f8fc97ad1eb6e62a2c315e97a
David Papamichael <davidpapamichael@gmail.com>
2020-05-11 09:34:16 +0100

    default quantity of 1 added to select_items

--- lib/order.rb
+++ lib/order.rb
@@ -1,13 +1,15 @@
 class Order
 
-  attr_reader :basket
+  attr_reader :basket, :menu
 
   def initialize(menu = Menu.new(items))
     @menu = menu
     @basket = {}
   end
 
-  def select_items(item, quantity)
+  def select_items(item, quantity = 1)
+
+
     if !basket.has_key?(item.to_sym)
        basket[item.to_sym] = quantity 
     else  
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -11,9 +11,18 @@ describe Order do
      3. times {subject.select_items("kebab", 1)}
      2.times {subject.select_items("pizza", 1)}
      subject.select_items("curry", 1)
- 
      expect(subject.basket.length).to eq(3)
      expect(subject.basket).to eq(kebab: 3, pizza: 2, curry: 1)
     end
+
+    it "selects items avaliable from the menu with 1 quantity as a default" do
+      subject.select_items("kebab")
+      subject.select_items("pizza")
+      subject.select_items("curry")
+      expect(subject.basket.length).to eq(3)
+      expect(subject.basket).to eq(kebab: 1, pizza: 1, curry: 1)
+     end
+
+ 
   end
 end
